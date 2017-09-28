9b527dc51ee6c240f51498572aa33a3059582551
Jitong-Liu <jitong.liu@outlook.com>
2017-09-28 08:12:22 +0000

    connect database to heroku

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+var url = process.env.DABATABASERURL || "mongodb://localhost/yelp_camp";
+mongoose.connect(url);
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
